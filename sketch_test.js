0368c9f6a272d99c0b3ff469f8a5902b2f5d2466
msoderblom <soderblom.matilda@gmail.com>
2020-03-18 15:56:17 +0100

    Added loading screen in container

--- sketch_test.js
+++ sketch_test.js
@@ -5,6 +5,7 @@ const playBtn = audioControl.querySelector(".playBtn");
 const pauseBtn = audioControl.querySelector(".pauseBtn");
 const restartAllBtn = audioControl.querySelector(".restartAllBtn");
 const restartMusicBtn = audioControl.querySelector(".restartMusicBtn");
+const loadingScreen = container.querySelector(".loadingScreen");
 
 let sketch = function(s) {
   let mySong;
@@ -14,15 +15,24 @@ let sketch = function(s) {
   let t4 = 0;
   let amp;
   let canvas;
-  let hasStarted = false;
+  let loading = true;
 
-  s.preload = function() {
-    s.soundFormats("mp3");
-    mySong = s.loadSound("./assets/drunk_in_love.mp3");
-  };
+  // s.preload = function() {
+  //   s.soundFormats("mp3");
+  //   mySong = s.loadSound("./assets/drunk_in_love.mp3");
+  // };
+
+  function error(err) {
+    console.log(err);
+  }
+  function soundLoaded() {
+    console.log("loaded");
+    loading = false;
+  }
+
   s.setup = function() {
-    // s.soundFormats("mp3");
-    // mySong = s.loadSound("./assets/drunk_in_love.mp3", loadScreen);
+    s.soundFormats("mp3");
+    mySong = s.loadSound("./assets/drunk_in_love.mp3", soundLoaded, error);
     canvas = s.createCanvas(700, 700);
     s.background(0);
     //fade = 0;
@@ -55,48 +65,51 @@ let sketch = function(s) {
     // });
   };
   s.draw = function() {
-    let volume = amp.getLevel();
-    let size1 = s.map(volume, 0, 1, 20, 200);
+    if (!loading) {
+      loadingScreen.classList.add("hide");
+      let volume = amp.getLevel();
+      let size1 = s.map(volume, 0, 1, 20, 200);
 
-    // Fyra variabler som används i beräkningen av maskarans koordinater.
-    // Musikens volym är med och påverkar förflyttningen.
-    t1 = t1 + 0.05 * volume;
-    t2 = t2 + 0.05 * volume;
-    t3 = t3 + 0.05 * volume;
-    t4 = t4 + 0.05 * volume;
+      // Fyra variabler som används i beräkningen av maskarans koordinater.
+      // Musikens volym är med och påverkar förflyttningen.
+      t1 = t1 + 0.05 * volume;
+      t2 = t2 + 0.05 * volume;
+      t3 = t3 + 0.05 * volume;
+      t4 = t4 + 0.05 * volume;
 
-    // Deklarerar x och y koordinater för maskarna. De tilldelas värdena av beräkningarna.
-    // Här beräknas noise av de tidigare variablerna. Noise genererar ett värde mellan 0 och 1.
-    let x1 = s.noise(t1);
-    let y1 = s.noise(t1 + 2); // Om man inte skriver +2 (eller någon annan siffra) så får x1 och y1 samma värde och masken rör sig bara diagonalt.
+      // Deklarerar x och y koordinater för maskarna. De tilldelas värdena av beräkningarna.
+      // Här beräknas noise av de tidigare variablerna. Noise genererar ett värde mellan 0 och 1.
+      let x1 = s.noise(t1);
+      let y1 = s.noise(t1 + 2); // Om man inte skriver +2 (eller någon annan siffra) så får x1 och y1 samma värde och masken rör sig bara diagonalt.
 
-    let x2 = s.noise(t2);
-    let y2 = s.noise(t2 + 2);
+      let x2 = s.noise(t2);
+      let y2 = s.noise(t2 + 2);
 
-    let x3 = s.noise(t3);
-    let y3 = s.noise(t3 + 2);
+      let x3 = s.noise(t3);
+      let y3 = s.noise(t3 + 2);
 
-    let x4 = s.noise(t4);
-    let y4 = s.noise(t4 + 2);
+      let x4 = s.noise(t4);
+      let y4 = s.noise(t4 + 2);
 
-    // Här översätts värdet genererat av noise till koordinater på sketchfönsteret.
-    // De olika maskarna är speglade mot varandra genom att width och heigt gentemot 0 vaierar.
-    x1 = s.map(x1, 0, 1, 0, s.width);
-    y1 = s.map(y1, 0, 1, 0, s.height);
+      // Här översätts värdet genererat av noise till koordinater på sketchfönsteret.
+      // De olika maskarna är speglade mot varandra genom att width och heigt gentemot 0 vaierar.
+      x1 = s.map(x1, 0, 1, 0, s.width);
+      y1 = s.map(y1, 0, 1, 0, s.height);
 
-    x2 = s.map(x2, 0, 1, s.width, 0);
-    y2 = s.map(y2, 0, 1, 0, s.height);
+      x2 = s.map(x2, 0, 1, s.width, 0);
+      y2 = s.map(y2, 0, 1, 0, s.height);
 
-    x3 = s.map(x3, 0, 1, 0, s.width);
-    y3 = s.map(y3, 0, 1, s.height, 0);
+      x3 = s.map(x3, 0, 1, 0, s.width);
+      y3 = s.map(y3, 0, 1, s.height, 0);
 
-    x4 = s.map(x4, 0, 1, s.width, 0);
-    y4 = s.map(y4, 0, 1, s.height, 0);
+      x4 = s.map(x4, 0, 1, s.width, 0);
+      y4 = s.map(y4, 0, 1, s.height, 0);
 
-    ritaMask(x1, y1, size1);
-    ritaMask(x2, y2, size1);
-    ritaMask(x3, y3, size1);
-    ritaMask(x4, y4, size1);
+      ritaMask(x1, y1, size1);
+      ritaMask(x2, y2, size1);
+      ritaMask(x3, y3, size1);
+      ritaMask(x4, y4, size1);
+    }
   };
 
   function ritaMask(x, y, size) {
@@ -117,23 +130,6 @@ let sketch = function(s) {
       playBtn.classList.toggle("hide");
       pauseBtn.classList.toggle("hide");
     }
-
-    // if (!hasStarted) {
-    //   mySong.play();
-    //   hasStarted = true;
-    //   console.log("first start");
-    // }
-
-    // if (mySong.isPlaying()) {
-    //   mySong.pause();
-    //   playBtn.classList.toggle("hide");
-    //   pauseBtn.classList.toggle("hide");
-    // } else if (mySong.isPaused()) {
-    //   playMode = "sustain";
-    //   mySong.play();
-    //   playBtn.classList.toggle("hide");
-    //   pauseBtn.classList.toggle("hide");
-    // }
   }
 
   function restartMusic() {
